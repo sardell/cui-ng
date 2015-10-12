131b94a348b5117bc23efd74a949de5b5b5889bd
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2015-10-12 15:12:29 -0500

    added some directives

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig ({
+    watch:{
+      css:{
+        files: 'assets/scss/**/*',
+        tasks: ['sass']
+      }
+    },
+    sass:{
+      dist:{
+        files:{
+          'assets/css/main.css': 'assets/scss/main.scss'
+        }
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+            src : [
+                '**/*.html',
+                '**/*.js',
+                '**/*.css'
+            ]
+        },
+        options: {
+          watchTask: true,
+          online: true,
+          server:{
+            baseDir: './'
+          }
+        }
+      }
+    },
+
+  });
+
+ 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['browserSync','watch']);
+}
