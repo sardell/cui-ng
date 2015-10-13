e870bcc407fc51ccf13153e3f2926217109f51d0
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2015-10-13 10:59:34 -0500

    adds next button and more styling

--- assets/scripts/app2.js
+++ assets/scripts/app2.js
@@ -1,4 +1,5 @@
 (function(angular){
+    'use strict';
 
     angular
     .module('app',[])
@@ -11,6 +12,7 @@
         app.logo='assets/img/logo.png';
 
         //for the wizard
+        app.step=1;
         app.organization={};
         app.organization.name='Thirdwave LLC';
         app.organization.divisions=['Web design','UI development','Wordpress development','Ruby development'];
@@ -34,7 +36,7 @@
                 attrs.topMenu!==undefined ? scope.cuiTopMenu=true : scope.cuiTopMenu=false;
                 
                 //set logo image
-                $logo = document.querySelector('.cui__header__logo');
+                var $logo = document.querySelector('.cui__header__logo');
                 $logo.style.backgroundImage = 'url("' + logo + '")';
             }
         }
@@ -57,23 +59,48 @@
     }])
 
     //cui-wizard
-    .directive('cuiWizard',[function(){
+    .directive('cuiWizard',['$timeout',function($timeout){
         return{
             restrict: 'E',
             link:function(scope,elem,attrs){
                 //init
-                $steps=elem.children();
-                var stepTitles=[],
-                    numberOfSteps=$steps.length;
-                for(i=0;i<numberOfSteps;i++){
-                    stepTitles[i]=$steps[i].attributes.title.value;
-                }
-                stepTitles.forEach(function(e,i){
-                    var div='<div class="step__indicator">' + stepTitles[numberOfSteps - (i+1)] + '</div>';
-                    elem.prepend(div);
-                })
+                var init = function(){
+                        scope.$steps=document.querySelectorAll('cui-wizard>step');
+                        scope.$indicatorContainer=document.querySelector('indicator-container');
+                        scope.next=function(){
+                            elem[0].attributes.step.value++;
+                            updateIndicator();
+                        };
+                        createIndicators();
+                        updateIndicator();
+                    },
+                    // creates indicators inside of <indicator-container>
+                    createIndicators = function(){
+                        var numberOfSteps=scope.$steps.length,
+                            stepTitles=[];
+                        for(var i=0;i<numberOfSteps;i++){
+                            stepTitles[i]=scope.$steps[i].attributes.title.value;
+                        }
+                        stepTitles.forEach(function(e,i){
+                            var div='<div class="step__indicator">' + stepTitles[i] + '</div>';
+                            scope.$indicatorContainer.innerHTML+=div;
+                        })
+                        scope.$indicators=document.querySelectorAll('.step__indicator');
+                    },
+                    // updates the current active indicator. Removes active class from other elements.
+                    updateIndicator = function(){
+                        $timeout(function(){
+                            var currentStep=elem[0].attributes.step.value;
+                            for(var i=0; i<scope.$steps.length ; i++){
+                                scope.$steps[i].classList.remove('active');
+                                scope.$indicators[i].classList.remove('active');
+                            }
+                            scope.$steps[currentStep-1].classList.add('active');
+                            scope.$indicators[currentStep-1].classList.add('active');
+                        });
+                    };
 
-
+                init();
             }
         }
     }]);
