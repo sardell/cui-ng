dd5175104d55a0918fe36d7289e449b29fc82535
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2015-10-29 15:37:43 -0500

    removes .tmp

--- dist/cui-ng.js
+++ dist/cui-ng.js
@@ -2,28 +2,30 @@
 
 	angular.module('cui-ng',[]);
 
-angular.module('cui-ng')
-.directive('cuiAvatar',[function(){
-    return{
-        restrict: 'E',
-        scope:{},
-        link:function(scope,elem,attrs){
-            scope.user={};
-            attrs.$observe('userAvatar',function(){
-                if(attrs.userAvatar!==''){
-                    scope.user.avatar=attrs.userAvatar;
-                    var background= 'url("' + scope.user.avatar + '")';
-                    angular.element(elem).css('background-image',background);
-                } 
-                else{
-                    scope.user.color='#AAA';
-                    var background= scope.user.color;
-                    angular.element(elem).css({'background-image':'none','background-color':background})
-                }
-            })
-        }
-    };
-}])
+(function(angular){
+    angular.module('cui-ng')
+    .directive('cuiAvatar',[function(){
+        return{
+            restrict: 'E',
+            scope:{},
+            link:function(scope,elem,attrs){
+                scope.user={};
+                attrs.$observe('userAvatar',function(){
+                    if(attrs.userAvatar!==''){
+                        scope.user.avatar=attrs.userAvatar;
+                        var background= 'url("' + scope.user.avatar + '")';
+                        angular.element(elem).css('background-image',background);
+                    } 
+                    else{
+                        scope.user.color='#AAA';
+                        var background= scope.user.color;
+                        angular.element(elem).css({'background-image':'none','background-color':background})
+                    }
+                })
+            }
+        };
+    }])
+})(angular);
 
 
 angular.module('cui-ng')
@@ -38,6 +40,7 @@ angular.module('cui-ng')
         }
     };
 }]);
+
 
 angular.module('cui-ng')
 .directive('cuiWizard',['$timeout','$compile','$window',function($timeout,$compile,$window){
@@ -209,7 +212,6 @@ angular.module('cui-ng')
         }
     };
 }]);
-
 
 angular.module('cui-ng')
 .directive('passwordValidation', [function(){		
