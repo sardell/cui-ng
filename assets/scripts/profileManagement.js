38bc3bc05ca8e1f99a36d6362a58cff797384cf6
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2015-11-23 11:08:55 -0600

    Adds local storage for user profiles

--- assets/scripts/profileManagement.js
+++ assets/scripts/profileManagement.js
@@ -1,11 +1,15 @@
 angular.module('app')
-.controller('profileManagementCtrl',['Person',function(Person){
+.controller('profileManagementCtrl',['Person', 'localStorageService', '$scope', function(Person, localStorageService, $scope){
     var profile=this;
-    profile.user=Person.add();
-    console.log(profile.user);
 
     profile.save=function(){
-        //Todo save profile.user to api
-        console.log(profile.user);
-    }
+    	//Todo save profile.user to api
+    	localStorageService.set('profile.user',$scope.profile.user)
+    };
+
+     var profileInStorage = localStorageService.get('profile.user');
+        profile.user = profileInStorage || Person.add();
+        $scope.$watch('profile.user',function(){
+            localStorageService.set('profile.user',$scope.profile.user)
+        }, true);
 }]);
