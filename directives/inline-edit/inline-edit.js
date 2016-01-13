35c194ee80a64fa42c23311aa02eb68626edcf31
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-01-13 11:16:33 -0600

    adds inline-edit directive

--- directives/inline-edit/inline-edit.js
+++ directives/inline-edit/inline-edit.js
@@ -1,9 +1,9 @@
 angular.module('cui-ng')
-.directive('cuiReview', ['$compile', function($compile){
+.directive('inlineEdit', ['$compile', function($compile){
   return {
     restrict: 'E',
     scope:{
-      model: '=model' 
+      model: '=' 
     },
     link: function(scope,ele,attrs){
       scope.edit=false;
@@ -16,18 +16,16 @@ angular.module('cui-ng')
       scope.saveInput=function(){
         scope.model=scope.editInput;
       };
-      scope.sayInput=function(){
-        console.log(scope.editInput);
-      };
 
       var element= $compile(
         '<p class="cui-expandable__review-item">{{"' + attrs.label + '"| translate}}: <span ng-if="!edit">{{model}}</span>' +
-        '<input type="text" ng-model="editInput" class="cui-input" ng-change="sayInput()" ng-init="matchModels()" ng-if="edit"/>' +
+        '<input type="text" ng-model="$parent.editInput" class="cui-input" ng-change="sayInput()" ng-init="matchModels()" ng-if="edit"/>' +
         '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit"> Edit</span>' + 
         '<span class="cui-link" ng-if="edit" ng-click="saveInput();toggleEdit();"> Save</span>'+
-        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()"> Cancel</span>'
+        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()"> Cancel</span></p>'
       )(scope);
       angular.element(ele[0]).html(element);
+
     }
   };
 }]);
--- dist/cui-ng.js
+++ dist/cui-ng.js
@@ -1124,41 +1124,6 @@ CuiIconService.$inject = ['config', '$http', '$q', '$templateCache'];
 
 
 angular.module('cui-ng')
-.directive('cuiReview', ['$compile', function($compile){
-  return {
-    restrict: 'E',
-    scope:{
-      model: '=model' 
-    },
-    link: function(scope,ele,attrs){
-      scope.edit=false;
-      scope.toggleEdit=function(){
-        scope.edit=!scope.edit;
-      };
-      scope.editInput='test';
-      scope.matchModels=function(){
-        scope.editInput=scope.model;
-      };
-      scope.saveInput=function(){
-        scope.model=scope.editInput;
-      };
-      scope.sayInput=function(){
-        console.log(scope.editInput);
-      };
-
-      var element= $compile(
-        '<p class="cui-expandable__review-item">{{"' + attrs.label + '"| translate}}: <span ng-if="!edit">{{model}}</span>' +
-        '<input type="text" ng-model="editInput" class="cui-input" ng-change="sayInput()" ng-if="edit"/>' +
-        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit"> Edit</span>' + 
-        '<span class="cui-link" ng-if="edit" ng-click="saveInput();toggleEdit();"> Save</span>'+
-        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()"> Cancel</span>'
-      )(scope);
-      angular.element(ele[0]).html(element);
-    }
-  };
-}]);
-
-angular.module('cui-ng')
 .directive('cuiWizard',['$timeout','$compile','$window','$rootScope',function($timeout,$compile,$window,$rootScope){
     return{
         restrict: 'E',
@@ -1457,6 +1422,38 @@ angular.module('cui-ng')
           checkErrors();
         });
       }
+    }
+  };
+}]);
+
+angular.module('cui-ng')
+.directive('inlineEdit', ['$compile', function($compile){
+  return {
+    restrict: 'E',
+    scope:{
+      model: '=' 
+    },
+    link: function(scope,ele,attrs){
+      scope.edit=false;
+      scope.toggleEdit=function(){
+        scope.edit=!scope.edit;
+      };
+      scope.matchModels=function(){
+        scope.editInput=scope.model;
+      };
+      scope.saveInput=function(){
+        scope.model=scope.editInput;
+      };
+
+      var element= $compile(
+        '<p class="cui-expandable__review-item">{{"' + attrs.label + '"| translate}}: <span ng-if="!edit">{{model}}</span>' +
+        '<input type="text" ng-model="$parent.editInput" class="cui-input" ng-change="sayInput()" ng-init="matchModels()" ng-if="edit"/>' +
+        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit"> Edit</span>' + 
+        '<span class="cui-link" ng-if="edit" ng-click="saveInput();toggleEdit();"> Save</span>'+
+        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()"> Cancel</span></p>'
+      )(scope);
+      angular.element(ele[0]).html(element);
+
     }
   };
 }]);
