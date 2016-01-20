d1b7caa7e9f2198fc7c76132d086ecf71bc36b31
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-01-20 10:53:45 -0600

    fixes minor issue with inline-edit

--- dist/cui-ng.js
+++ dist/cui-ng.js
@@ -1458,7 +1458,8 @@ angular.module('cui-ng')
     scope:{
       model: '=',
       type: '@',
-      options: '='
+      options: '=',
+      display: '='
     },
     link: function(scope,ele,attrs){
       scope.edit=false;
@@ -1493,10 +1494,16 @@ angular.module('cui-ng')
           'ng-init="matchModels()" ng-if="edit" ng-keypress="listenForEnter($event)" focus-if="focus"/>';
       };
 
+      var getDisplayValue=function(){
+        if(scope.display) return '{{display}}';
+        else return '{{model}}';
+      }
+
+
       var element= $compile(
-        '<p class="cui-expandable__review-item">' + getLabel() + ': <span ng-if="!edit">{{model}}</span>' +
+        '<p class="cui-expandable__review-item">' + getLabel() + ': <span ng-if="!edit">' + getDisplayValue() + '</span>' +
         getInput() +
-        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit"> Edit</span>' + 
+        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit"> Edit</span>' +
         '<span class="cui-link" ng-if="edit" ng-click="saveInput();toggleEdit();"> Save</span>'+
         '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()"> Cancel</span></p>'
       )(scope);
@@ -2856,130 +2863,130 @@ angular.module('cui-ng')
   );
 }]);
 
-  // how to use:
-  // .run(['$rootScope', '$state', 'cui.authorization.routing' function($rootScope,$state){
-  //   $rootScope.$on('$stateChangeStart', function(event, toState){
-  //     cui.authorization.routing($state,toState,userEntitlements);
-  //   })
-  // }])
-  //
-  // userEntitlements must be an array of entitlements.
-  // It will redirect to a state called 'login' if no user is defined
-  // It will redirect to a state called 'notAuthorized' if the user doesn't have permission
-  angular.module('cui.authorization',[])
-  .factory('cui.authorization.routing', ['cui.authorization.authorize', '$timeout',
-    function (authorize,$timeout){
-      var routing = function($rootScope, $state, toState, toParams, fromState, fromParams, userEntitlements){
-        var authorized;
-        if (toState.access !== undefined) {
-          // console.log('Access rules for this route: \n' +
-          // 'loginRequired: ' + toState.access.loginRequired + '\n' +
-          // 'requiredEntitlements: ' + toState.access.requiredEntitlements);
-            authorized = authorize.authorize(toState.access.loginRequired,
-                 toState.access.requiredEntitlements, toState.access.entitlementType, userEntitlements);
-            // console.log('authorized: ' + authorized);
-            if (authorized === 'login required') {
-                $timeout(function(){
-                  $state.go('login',toParams).then(function() {
-                    $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-                  });
-                });
-            } else if (authorized === 'not authorized') {
-                $timeout(function(){
-                  $state.go('notAuthorized',toParams).then(function() {
-                      $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-                  });
-                });
-            }
-            else if(authorized === 'authorized'){
-              $timeout(function(){
-                $state.go(toState.name,toParams,{notify:false}).then(function() {
-                    $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-                });
+// how to use:
+// .run(['$rootScope', '$state', 'cui.authorization.routing' function($rootScope,$state){
+//   $rootScope.$on('$stateChangeStart', function(event, toState){
+//     cui.authorization.routing($state,toState,userEntitlements);
+//   })
+// }])
+//
+// userEntitlements must be an array of entitlements.
+// It will redirect to a state called 'login' if no user is defined
+// It will redirect to a state called 'notAuthorized' if the user doesn't have permission
+angular.module('cui.authorization',[])
+.factory('cui.authorization.routing', ['cui.authorization.authorize', '$timeout',
+function (authorize,$timeout){
+  var routing = function($rootScope, $state, toState, toParams, fromState, fromParams, userEntitlements){
+    var authorized;
+    if (toState.access !== undefined) {
+      // console.log('Access rules for this route: \n' +
+      // 'loginRequired: ' + toState.access.loginRequired + '\n' +
+      // 'requiredEntitlements: ' + toState.access.requiredEntitlements);
+        authorized = authorize.authorize(toState.access.loginRequired,
+             toState.access.requiredEntitlements, toState.access.entitlementType, userEntitlements);
+        // console.log('authorized: ' + authorized);
+        if (authorized === 'login required') {
+            $timeout(function(){
+              $state.go('login',toParams).then(function() {
+                $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
               });
-            }
+            });
+        } else if (authorized === 'not authorized') {
+            $timeout(function(){
+              $state.go('notAuthorized',toParams).then(function() {
+                  $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+              });
+            });
         }
-        else {
-          $state.go(toState.name,toParams,{notify:false}).then(function() {
-              $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+        else if(authorized === 'authorized'){
+          $timeout(function(){
+            $state.go(toState.name,toParams,{notify:false}).then(function() {
+                $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+            });
           });
         }
-      };
+    }
+    else {
+      $state.go(toState.name,toParams,{notify:false}).then(function() {
+          $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+      });
+    }
+  };
 
-      return routing;
-    }])
+  return routing;
+}])
 
 
-  .factory('cui.authorization.authorize', [
-    function () {
-     var authorize = function (loginRequired, requiredEntitlements, entitlementType, userEntitlements) {
-        var loweredPermissions = [],
-            hasPermission = true,
-            permission, i,
-            result='not authorized';
-        entitlementType = entitlementType || 'atLeastOne';
-        if (loginRequired === true && userEntitlements === undefined) {
-            result = 'login required';
-        } else if ((loginRequired === true && userEntitlements !== undefined) &&
-            (requiredEntitlements === undefined || requiredEntitlements.length === 0)) {
-            // Login is required but no specific permissions are specified.
-            result = 'authorized';
-        } else if (requiredEntitlements) {
-            angular.forEach(userEntitlements, function (permission) {
-                loweredPermissions.push(permission.toLowerCase());
-            });
-            for (i = 0; i < requiredEntitlements.length; i++) {
-                permission = requiredEntitlements[i].toLowerCase();
+.factory('cui.authorization.authorize', [
+function () {
+ var authorize = function (loginRequired, requiredEntitlements, entitlementType, userEntitlements) {
+    var loweredPermissions = [],
+        hasPermission = true,
+        permission, i,
+        result='not authorized';
+    entitlementType = entitlementType || 'atLeastOne';
+    if (loginRequired === true && userEntitlements === undefined) {
+        result = 'login required';
+    } else if ((loginRequired === true && userEntitlements !== undefined) &&
+        (requiredEntitlements === undefined || requiredEntitlements.length === 0)) {
+        // Login is required but no specific permissions are specified.
+        result = 'authorized';
+    } else if (requiredEntitlements) {
+        angular.forEach(userEntitlements, function (permission) {
+            loweredPermissions.push(permission.toLowerCase());
+        });
+        for (i = 0; i < requiredEntitlements.length; i++) {
+            permission = requiredEntitlements[i].toLowerCase();
 
-                if (entitlementType === 'all') {
-                    hasPermission = hasPermission && loweredPermissions.indexOf(permission) > -1;
-                    // if all the permissions are required and hasPermission is false there is no point carrying on
-                    if (hasPermission === false) {
-                        break;
-                    }
-                } else if (entitlementType === 'atLeastOne') {
-                    hasPermission = loweredPermissions.indexOf(permission) > -1;
-                    // if we only need one of the permissions and we have it there is no point carrying on
-                    if (hasPermission) {
-                        break;
-                    }
+            if (entitlementType === 'all') {
+                hasPermission = hasPermission && loweredPermissions.indexOf(permission) > -1;
+                // if all the permissions are required and hasPermission is false there is no point carrying on
+                if (hasPermission === false) {
+                    break;
+                }
+            } else if (entitlementType === 'atLeastOne') {
+                hasPermission = loweredPermissions.indexOf(permission) > -1;
+                // if we only need one of the permissions and we have it there is no point carrying on
+                if (hasPermission) {
+                    break;
                 }
             }
-            result = hasPermission ?
-                     'authorized' :
-                     'not authorized';
         }
-        return result;
+        result = hasPermission ?
+                 'authorized' :
+                 'not authorized';
+    }
+    return result;
+};
+
+    return {
+        authorize: authorize
     };
+}])
 
-        return {
-         authorize: authorize
-        };
-  }])
-
-  .directive('cuiAccess',['cui.authorization.authorize',function(authorize){
-      return{
-          restrict:'A',
-          scope: {
+.directive('cuiAccess',['cui.authorization.authorize',function(authorize){
+    return{
+        restrict:'A',
+        scope: {
             userEntitlements:'=',
             cuiAccess:'='
-          },
-          link: function(scope,elem,attrs){
-              scope.loginRequired= true;
-              scope.requiredEntitlements= scope.cuiAccess.requiredEntitlements || [];
-              scope.entitlementType= scope.cuiAccess.entitlementType || 'atLeastOne';
-              elem=angular.element(elem);
-              scope.$watch('userEntitlements',function(){
-                  var authorized=authorize.authorize(scope.loginRequired, scope.requiredEntitlements, scope.entitlementType, scope.userEntitlements);
-                  if(authorized!=='authorized'){
-                      elem.addClass('hide');
-                  }
-                  else{
-                      elem.removeClass('hide');
-                  }
-              });
-          }
-      };
-  }]);
+        },
+        link: function(scope,elem,attrs){
+            scope.loginRequired= true;
+            scope.requiredEntitlements= scope.cuiAccess.requiredEntitlements || [];
+            scope.entitlementType= scope.cuiAccess.entitlementType || 'atLeastOne';
+            elem=angular.element(elem);
+            scope.$watch('userEntitlements',function(){
+                var authorized=authorize.authorize(scope.loginRequired, scope.requiredEntitlements, scope.entitlementType, scope.userEntitlements);
+                if(authorized!=='authorized'){
+                  elem.addClass('hide');
+                }
+                else{
+                    elem.removeClass('hide');
+                }
+            });
+        }
+    };
+}]);
 
 })(angular);
