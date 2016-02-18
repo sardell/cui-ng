17e07ca3228fa077729b7a03268a3156bf059778
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-02-18 14:00:31 -0600

    fixes expandable behaviour when expanded by default

--- dist/cui-ng.js
+++ dist/cui-ng.js
@@ -850,9 +850,9 @@ angular.module('cui-ng')
         scope: true,
         link:function(scope,elem,attrs){
             var expandableBody=angular.element(elem).children('cui-expandable-body');
-            expandableBody.hide();
-            scope.toggleExpand=function(){
-                elem.toggleClass('expanded');
+            expandableBody.hide(); // hide the body by default
+            scope.toggleExpand=function(toggleClass){
+                if(toggleClass==undefined || toggleClass!=false) elem.toggleClass('expanded');
                 expandableBody.animate({'height':'toggle'},300,'linear');
             };
             scope.expand=function(){
@@ -861,8 +861,14 @@ angular.module('cui-ng')
             scope.collapse=function(){
             	if(scope.expanded) scope.toggleExpand();
             };
-            scope.$watch(function() {return elem.attr('class'); }, function(newValue){
-                if(newValue.indexOf('expanded')>-1) scope.expanded=true;
+            scope.$watch(function() {return elem.attr('class'); }, function(newValue,oldValue){
+                if(oldValue===newValue && newValue.indexOf('expanded')>-1 ){ // if the element the expanded class put in by default
+                    scope.expanded=true;
+                    scope.toggleExpand(false);
+                }
+                else if(oldValue && newValue.indexOf('expanded')>-1){ // if the element has the expanded class now but didn't before
+                    scope.expanded=true;
+                }
                 else scope.expanded=false;
             });
         }
