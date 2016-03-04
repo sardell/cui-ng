8a999b0bb018e90adb87f399563a5ec611a3414e
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-03-04 11:02:39 -0600

    inline edit now using string.concat

--- dist/cui-ng.js
+++ dist/cui-ng.js
@@ -1628,21 +1628,23 @@ angular.module('cui-ng')
       };
 
       var getLabel=function(){
-        if(attrs.label!==undefined) return '{{"' + attrs.label + '"| translate}}';
+        var label='';
+        if(attrs.label!==undefined) return label.concat('{{"', attrs.label,'"| translate}}');
         else if(attrs.name!==undefined) return attrs.name;
         else console.log('Inline-edit needs 1 of the following attributes: label or name.');
       };
 
       var getInput=function(){
         attrs.type=attrs.type || 'text';
-        if(attrs.type==='dropdown') return '<select ng-model="$parent.editInput" class="' + inputClass + '"' +
-          'ng-init="matchModels()" ng-options="' + attrs.optionsExpression + '" ng-if="edit"></select>';
-        else if(attrs.type==='auto-complete') return '<div auto-complete selected-object="$parent.editInput" local-data="localData"' +
-          ' search-fields="' + attrs.searchFields + ' " title-field="' + attrs.titleField + '" input-class="'+ inputClass + '" '+
-          ' match-class="highlight" ng-init="matchModels()" auto-match="true"' +
-          ' ng-if="edit" ng-keypress="parseKeyCode($event)" initial-value="$parent.editInput.title"></div>';
-        return '<input type="' + attrs.type + '" ng-model="$parent.editInput" class="'+ inputClass +'" ' +
-          'ng-init="matchModels()" ng-if="edit" ng-keyup="parseKeyCode($event)" focus-if="focus"/>';
+        var input='';
+        if(attrs.type==='dropdown') return input.concat('<select ng-model="$parent.editInput" class="',inputClass,'"',
+          'ng-init="matchModels()" ng-options="',attrs.optionsExpression,'" ng-if="edit"></select>');
+        else if(attrs.type==='auto-complete') return input.concat('<div auto-complete selected-object="$parent.editInput" local-data="localData"',
+          ' search-fields="',attrs.searchFields,'" title-field="',attrs.titleField,'" input-class="',inputClass,'" ',
+          ' match-class="highlight" ng-init="matchModels()" auto-match="true"',
+          ' ng-if="edit" ng-keypress="parseKeyCode($event)" initial-value="$parent.editInput.title"></div>');
+        return input.concat('<input type="',attrs.type,'" ng-model="$parent.editInput" class="',inputClass,'" ',
+          'ng-init="matchModels()" ng-if="edit" ng-keyup="parseKeyCode($event)" focus-if="focus"/>');
       };
 
       var getDisplayValue=function(){
@@ -1655,12 +1657,13 @@ angular.module('cui-ng')
       scope.$watch('display',getDisplayValue);
       scope.$watch('model',getDisplayValue);
 
+      var elementMarkup='';
       var element= $compile(
-        '<div class="' + wrapperClass + '"><span class="' + labelClass + '">' + getLabel() + ':</span><span ng-if="!edit" class="' + valueClass + '">' +
-        '{{displayValue}}' + '</span>' + getInput() + '</div>' +
-        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit">{{ "edit" | translate }}</span>' +
-        '<span class="cui-button" ng-if="edit" ng-click="saveInput();toggleEdit();">{{ "update" | translate }}</span>'+
-        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()">{{ "cancel" | translate }}</span>'
+        elementMarkup.concat('<div class="',wrapperClass,'"><span class="',labelClass,'">',getLabel(),':</span><span ng-if="!edit" class="',valueClass,'">',
+        '{{displayValue}}','</span>',getInput() ,'</div>',
+        '<span class="cui-link" ng-click="toggleEdit()" ng-if="!edit">{{ "edit" | translate }}</span>',
+        '<span class="cui-button" ng-if="edit" ng-click="saveInput();toggleEdit();">{{ "update" | translate }}</span>',
+        '<span class="cui-link" ng-if="edit" ng-click="toggleEdit()">{{ "cancel" | translate }}</span>')
       )(scope);
       angular.element(ele[0]).html(element);
 
